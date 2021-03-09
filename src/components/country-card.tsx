44e76e41bf8079c7a8c1973ba6c2fb91d6dcf509
Andrei_Shauniou <andrei.shauniou@kronos.com>
2021-03-10 01:00:05 +0300

    feat: add template for new components. Add *.scss

---
+++ src/components/country-card.tsx
@@ -0,0 +1,22 @@
+import '../css/country-card.scss';
+
+import React from 'react';
+import { Link, HashRouter as Router } from 'react-router-dom';
+
+const CountryCard: React.FC<any> = () => (
+  // <div className="country-card-wrapper">
+  <Router>
+    <Link to="/country/1">
+      <div className="country-card">
+        <span className="title_cart">Franse</span>
+        <span className="sub-title_cart">Paris</span>
+        <span className="more">
+          start your journey...
+        </span>
+      </div>
+    </Link>
+  </Router>
+  // </div>
+);
+
+export default CountryCard;
--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,5 @@
+import './css/main.scss';
+
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 

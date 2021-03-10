1ad0f0d959f68294380d94089d0e4a01b30e9f0f
Mikhail Shemchuk <mihey.by@gmail.com>
2021-03-10 21:20:10 +0300

    feat: add file countriesList.ts with 9 countries

--- src/models/country.ts
+++ src/models/country.ts
@@ -1,15 +1,17 @@
-import Lang from './lang';
+// import Lang from './lang';
 import IPlace from './place';
 import IVideo from './video';
 
 export default interface ICountry {
-  id: number
+  id: number;
   name: string;
   capital: string;
+  capitalCoordinates: { lat: number, lon: number },
   photo: string;
   currency: string;
   description: string;
   places: IPlace[];
   videos: IVideo[];
-  lang: Lang;
+  lang: string; // EN, RU, BY
+  // lang: Lang;
 }

207c155e60dc170ccff63cb51d515fee52eefc2c
seha1 <seha1@yandex.ru>
2021-03-13 21:11:25 +0300

    feat: create CountryPageDescription component feat: set the styles for the component CountryPageDescription

--- src/components/country-page.tsx
+++ src/components/country-page.tsx
@@ -5,16 +5,17 @@ import CountryPageMap from './countryPageComponents/countryPageMap';
 import CountryPageVideo from './countryPageComponents/countryPageVideo';
 
 import '../css/country-page-style.scss';
+import CountryPageDescription from './countryPageComponents/CountryPageDescription';
 
 const CountryPage: React.FC = () => (
   <>
     <section className="country_page_wrapper">
       <CountryPageInformer />
       <CountryPageGallery />
+      <CountryPageDescription />
       <CountryPageMap />
       <CountryPageVideo />
     </section>
-
   </>
 );
 

0f28f86182e8642d73bb7e3136502507aefc2c03
durden666 <thanks.vlad12@gmail.com>
2021-03-14 13:54:43 +0300

    feat: add video-component

--- src/components/countryPageComponents/countryPageVideo.tsx
+++ src/components/countryPageComponents/countryPageVideo.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import '../../css/country-page-vedeo.scss';
+// @ts-ignore
+import { Player, ControlBar } from 'video-react';
+import 'video-react/dist/video-react.scss';
 
-const CountryPageVideo: React.FC = () => (
-  <>
-    <div className="country_page_video">
-      {/* eslint-disable-next-line jsx-a11y/media-has-caption */}
-      <video src="https://www.youtube.com/watch?v=8yb9zpDia40" width="100%">
-        Ваш браузер не поддерживает это видео.
-      </video>
-    </div>
-  </>
-);
+const Video = () => (
+  <Player
+    playsInline
+    poster="https://video-react.js.org/assets/poster.png"
+    src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4">
+    <ControlBar autoHide className="my-class" />
+  </Player>
+)
 
-export default CountryPageVideo;
+
+export default Video;

2e029668143a75cea60fb128278e4ef969004db2
durden666 <thanks.vlad12@gmail.com>
2021-03-14 16:55:57 +0300

    feat: add gallery-component

--- src/components/countryPageComponents/countryPageGallery.tsx
+++ src/components/countryPageComponents/countryPageGallery.tsx
@@ -1,5 +1,8 @@
 import '../../css/country-page-gallery.scss';
-import React from 'react';
+import React, { useEffect, useState } from 'react'
+import axios from "axios";
+import ReactImageGallery from "react-image-gallery";
+import "react-image-gallery/styles/scss/image-gallery.scss";
 import IPlace from '../../models/place';
 
 interface ICountryPageGalleryProps {
@@ -9,29 +12,31 @@ interface ICountryPageGalleryProps {
 const CountryPageGallery: React.FC<ICountryPageGalleryProps> = (props: ICountryPageGalleryProps) => {
   const { places } = props;
 
-  return (
-    <>
-      <div className="gallery">
-        <div className="left_button">
-          <div className="left_button_tap">
-            <div className="left_top_hr" />
-            <div className="left_bottom_hr" />
-          </div>
-        </div>
-        <div className="body_gallery">
-          {places.map((p) => (
-            <div key={p.id}>{p.name}</div>
-          ))}
-        </div>
-        <div className="right_button">
-          <div className="right_button_tap">
-            <div className="right_top_hr" />
-            <div className="right_bottom_hr" />
-          </div>
-        </div>
-      </div>
-    </>
-  );
+  const [images, setImages] = useState(null);
+  const shouldCancel = false;
+
+  useEffect((): any => {
+    const call = async () => {
+      const res = await axios.get(
+        "https://google-photos-album-demo2.glitch.me/4eXXxxG3rYwQVf948"
+      );
+      if (!shouldCancel && res.data && res.data.length > 0) {
+        setImages(
+          // @ts-ignore
+          res.data.map(url => ({        
+            thumbnailLabel: 'Grand Canyon',
+            description: 'Grand Canyon',
+            original: `${url}=w1024`,
+            thumbnail: `${url}=w100`,
+          }))
+        );
+      }
+    };
+    call();
+  }, [shouldCancel]);
+  
+  // @ts-ignore
+  return images ? <ReactImageGallery items={images} /> : null;
 };
 
 export default CountryPageGallery;

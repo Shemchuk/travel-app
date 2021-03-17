df6def6723f6422d87cb7391fe8ebcd0fcbebf75
Mikhail Shemchuk <mihey.by@gmail.com>
2021-03-17 23:03:25 +0300

    feat: implement map

--- src/components/countryPageComponents/countryPageMap.tsx
+++ src/components/countryPageComponents/countryPageMap.tsx
@@ -1,17 +1,54 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import mapboxgl from 'mapbox-gl';
+import { connect } from 'react-redux';
+
+import IState from '../../store/state';
 import '../../css/country-page-map.scss';
 
-const countryPageMap: React.FC = () => (
-  <>
-    <div className="country_page_map">
-      <script
-        type="text/javascript"
-        charSet="utf-8"
-        async
-        src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A6b9da30b9ecdb833f4fb348e3d7a334d6a90085bdc04295aea25b173e7da608d&amp;width=993&amp;height=250&amp;lang=ru_RU&amp;scroll=true"
-      />
-    </div>
-  </>
-);
+const MAPBOX_TOKEN =
+  'pk.eyJ1IjoibWljaGFlbHNoIiwiYSI6ImNraXFkdnZ0ajF0bm4ycmxiM3k0MXRvcjMifQ.Yf1Olmco7KyZFm-rRvcPaw';
 
-export default countryPageMap;
+const createMap = (markerPoint: any) => {
+  mapboxgl.accessToken = MAPBOX_TOKEN;
+
+  const map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/dark-v10',
+    zoom: 2,
+    minZoom: 1,
+    center: markerPoint,
+  });
+
+  const nav = new mapboxgl.NavigationControl({ visualizePitch: true });
+  const scale = new mapboxgl.ScaleControl({
+    maxWidth: 80,
+    unit: 'metric',
+  });
+
+  map.addControl(new mapboxgl.FullscreenControl());
+  map.addControl(nav, 'top-left');
+  map.addControl(scale);
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const marker = new mapboxgl.Marker()
+    .setLngLat(markerPoint)
+    .addTo(map);
+}
+
+const CountryPageMap: React.FC = (props: any) => {
+  const { country } = props;
+  const coordinates = country.capitalCoordinates;
+  const markerPoint = [coordinates.lon, coordinates.lat];
+
+  useEffect(() => {
+    createMap(markerPoint);
+  });
+
+  return  (
+      <div className="country_page_map" id="map" />
+    );
+}
+
+const mapStateToProps = (state: IState) => ({
+  country: state.selectedCountry,
+});
+export default connect(mapStateToProps)(CountryPageMap);
